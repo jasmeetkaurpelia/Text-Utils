9208d0c25986a396c0f2f5ebbb6553b19ce8e103
Jasmeet Kaur <130532281+jasmeetkaurpelia@users.noreply.github.com>
2023-11-21 20:56:55 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,8 +6,10 @@ export default function Alert(props) {
         return lowerword.charAt(0).toUpperCase() + lowerword.slice(1);
     }
     return (
-            props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <div style={{height: '50px'}}>
+            {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 <strong>{capitalise(props.alert.type)}: </strong>{props.alert.msg}
-            </div>
+            </div>}
+        </div>
     )
 }
