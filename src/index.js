58d015221bafab639b2b75972ca0df58c6995274
Jasmeet Kaur <130532281+jasmeetkaurpelia@users.noreply.github.com>
2024-08-14 13:24:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import Router here
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component with Router
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/Text-Utils"> {/* Set basename for GitHub Pages */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
-ReactDOM.render(
-  <Router basename="/Text-Utils">
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// Measure performance in your app
 reportWebVitals();
